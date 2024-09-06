a7666d0510bc17b05dce8d87d7d1df27bc350a22
firm0001 <106458550+firm0001@users.noreply.github.com>
2024-09-06 17:42:31 -0400

    Update utilities.lua

--- utils/utilities.lua
+++ utils/utilities.lua
@@ -1,3 +1,10 @@
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local Utilities = {}
 Utilities.Events = {
     RenderStepped = RunService.RenderStepped,
@@ -8,10 +15,11 @@ Utilities.Events = {
     Chatted = LocalPlayer.Chatted,
     ChildAdded = Workspace.ChildAdded,
     DescendantAdded = Workspace.DescendantAdded,
-    IgnoredDescendantAdded = Workspace.Ignored.DescendantAdded,
+    IgnoredDescendantAdded = Workspace:FindFirstChild("Ignored") and Workspace.Ignored.DescendantAdded or nil,
     InputBegan = UserInputService.InputBegan,
     InputEnded = UserInputService.InputEnded,
     ReplicatedStorageChildAdded = ReplicatedStorage.ChildAdded,
     ReplicatedStorageDescendantAdded = ReplicatedStorage.DescendantAdded
 }
+
 return Utilities
