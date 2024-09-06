bf0186ca13d1fbdc6a7470a8a1da2e62047f01e6
firm0001 <106458550+firm0001@users.noreply.github.com>
2024-09-06 17:46:27 -0400

    Update modules.lua

--- utils/modules.lua
+++ utils/modules.lua
@@ -2,7 +2,28 @@ local Modules = {}
 local connections = {}
 
 local repo = 'https://raw.githubusercontent.com/firm0001/sigma/main/'
-local Utilities = loadstring(game:HttpGet(repo .. "utils/utilities.lua"))()
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Utilities = {}
+Utilities.Events = {
+    RenderStepped = RunService.RenderStepped,
+    Heartbeat = RunService.Heartbeat,
+    Stepped = RunService.Stepped,
+    PlayerAdded = Players.PlayerAdded,
+    PlayerRemoving = Players.PlayerRemoving,
+    Chatted = LocalPlayer.Chatted,
+    ChildAdded = Workspace.ChildAdded,
+    DescendantAdded = Workspace.DescendantAdded,
+    IgnoredDescendantAdded = Workspace:FindFirstChild("Ignored") and Workspace.Ignored.DescendantAdded or nil,
+    InputBegan = UserInputService.InputBegan,
+    InputEnded = UserInputService.InputEnded,
+    ReplicatedStorageChildAdded = ReplicatedStorage.ChildAdded,
+    ReplicatedStorageDescendantAdded = ReplicatedStorage.DescendantAdded
+}
 local Error = loadstring(game:HttpGet(repo .. "utils/error.lua"))()
 
 function Modules:New(class, properties)
